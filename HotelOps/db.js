bc3903a4dfe638af03297592dd694b7a938f641e
GouravRathi <Gourav.Rathi@nilehospitality.com>
2026-08-31 12:43:15 +0530

    Env chnages for loacl

--- HotelOps/db.js
+++ HotelOps/db.js
@@ -1,15 +1,23 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+const isLocal =
+    process.env.DB_HOST === "localhost" ||
+    process.env.DB_HOST === "127.0.0.1";
 const pool = new Pool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: Number(process.env.DB_PORT),
-    ssl: {
-        rejectUnauthorized: false
-    }
+    // ssl: {
+    //     rejectUnauthorized: false
+    // }
+     ssl: isLocal
+        ? false
+        : {
+              rejectUnauthorized: false
+          }
 });
 
 const connectDB = async () => {
