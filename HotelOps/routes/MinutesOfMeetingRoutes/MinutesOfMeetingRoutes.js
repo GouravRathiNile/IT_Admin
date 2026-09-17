58d57898cb93323f625d4cba5b037bc63fe1d194
GouravRathi <Gourav.Rathi@nilehospitality.com>
2026-09-17 17:11:02 +0530

    MOM Reports complete

--- HotelOps/routes/MinutesOfMeetingRoutes/MinutesOfMeetingRoutes.js
+++ HotelOps/routes/MinutesOfMeetingRoutes/MinutesOfMeetingRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const authenticateToken = require("../../middleware/authMiddleware");
-const { getMOMById,saveMOM,deleteMOM,getAllMOM,updateMOMStatus } = require("../../controllers/MinutesOfMeetingController/MinutesOfMeetingController");
+const { getMOMById,saveMOM,deleteMOM,getAllMOM,updateMOMStatus,getMOMTitles,getMOMActions,getMOMSummaryReport,getMOMResponsiblePersonReport,getMOMActionDetailReport,getMOMListPdf } = require("../../controllers/MinutesOfMeetingController/MinutesOfMeetingController");
 const router = express.Router();
 
 // ============================================================ CRUD Operations
@@ -9,5 +9,17 @@ router.get("/MOMById/:MeetingID",authenticateToken,getMOMById);
 router.delete("/Delete",authenticateToken,deleteMOM,);
 // ============================================================ MOM List
 router.get("/MOMList",authenticateToken,getAllMOM,);
+// ============================================================ Update MOM Status
 router.put("/UpdateMOMStatus",authenticateToken,updateMOMStatus,);
+// ============================================================ Title And Action 
+router.get("/MOMTitles",authenticateToken,getMOMTitles,);
+router.get("/MOMActions",authenticateToken,getMOMActions,);
+// ============================================================ Reports
+router.get("/MOMSummaryReport",authenticateToken,getMOMSummaryReport,);
+router.get("/ResponsiblePersonReport",authenticateToken,getMOMResponsiblePersonReport,);
+router.get("/ActionDetailsReport",authenticateToken,getMOMActionDetailReport,);
+// ============================================================ Pdfs
+router.get("/MOMListPdf",authenticateToken,getMOMListPdf,);
+
+
 module.exports = router;
